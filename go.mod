1d219ba3b9d521099606741a89fd40a405429ef9
lightlus <1211200376@qq.com>
2023-11-25 17:34:48 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,38 @@
+module github.com/lightlus/polevpn_server
+
+go 1.21
+
+require (
+	github.com/go-ldap/ldap/v3 v3.4.6
+	github.com/gorilla/websocket v1.5.1
+	github.com/lightlus/anyvalue v1.0.6
+	github.com/lightlus/elog v1.1.1
+	github.com/lightlus/h3conn v1.1.0
+	github.com/lightlus/netstack v1.2.0
+	github.com/lightlus/water v1.0.4
+	github.com/quic-go/quic-go v0.40.0
+)
+
+require (
+	github.com/Azure/go-ntlmssp v0.0.0-20221128193559-754e69321358 // indirect
+	github.com/go-asn1-ber/asn1-ber v1.5.5 // indirect
+	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
+	github.com/google/btree v1.0.0 // indirect
+	github.com/google/pprof v0.0.0-20210407192527-94a9f03dee38 // indirect
+	github.com/google/uuid v1.3.1 // indirect
+	github.com/onsi/ginkgo/v2 v2.9.5 // indirect
+	github.com/quic-go/qpack v0.4.0 // indirect
+	github.com/quic-go/qtls-go1-20 v0.4.1 // indirect
+	github.com/vmihailenco/msgpack/v5 v5.0.0 // indirect
+	github.com/vmihailenco/tagparser v0.1.2 // indirect
+	go.uber.org/mock v0.3.0 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/exp v0.0.0-20221205204356-47842c84f3db // indirect
+	golang.org/x/mod v0.11.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e // indirect
+	golang.org/x/tools v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+)
---
+++ local_login_check.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/go-ldap/ldap/v3"
+	"github.com/lightlus/anyvalue"
+)
+
+const (
+	MAX_PASSWORD_LENGHT = 64
+)
+
+type User struct {
+	Uid           uint64
+	Email         string
+	Vip           int
+	VipExpireTime uint64
+	LastLoginTime uint64
+}
+
+type LocalLoginChecker struct {
+}
+
+func NewLocalLoginChecker() *LocalLoginChecker {
+	return &LocalLoginChecker{}
+}
+
+func (llc *LocalLoginChecker) CheckLogin(user string, pwd string) error {
+
+	var err error
+
+	if Config.Has("auth.file") {
+		err = llc.checkFileLogin(user, pwd)
+	}
+
+	if err == nil {
+		return nil
+	}
+
+	if Config.Has("auth.http") {
+		err = llc.checkHttpLogin(user, pwd)
+	}
+
+	if err == nil {
+		return nil
+	}
+
+	if Config.Has("auth.ldap") {
+		err = llc.checkLDAPLogin(user, pwd)
+	}
+
+	return err
+
+}
+
+func (llc *LocalLoginChecker) checkFileLogin(user string, pwd string) error {
+
+	filePath := Config.Get("auth.file.path").AsStr()
+
+	f, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	br := bufio.NewReader(f)
+	for {
+		line, _, c := br.ReadLine()
+		if c == io.EOF {
+			break
+		}
+		userPassword := strings.Trim(string(line), "\n\r")
+		userPasswordArr := strings.Split(userPassword, ",")
+		if len(userPasswordArr) != 2 {
+			continue
+		}
+		if userPasswordArr[0] == user && userPasswordArr[1] == pwd {
+			return nil
+		}
+	}
+	return errors.New("user or password incorrect")
+}
+
+func (llc *LocalLoginChecker) checkHttpLogin(user string, pwd string) error {
+
+	req := anyvalue.New()
+
+	req.Set("user", user)
+	req.Set("pwd", pwd)
+	data, _ := req.EncodeJson()
+
+	client := http.Client{Timeout: time.Duration(Config.Get("auth.http.timeout").AsInt()) * time.Second}
+	request, err := http.NewRequest(http.MethodPost, Config.Get("auth.http.url").AsStr(), bytes.NewReader(data))
+
+	if err != nil {
+		return err
+	}
+
+	resp, err := client.Do(request)
+
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		data, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nil
+		}
+		return errors.New(string(data))
+	}
+	return nil
+}
+
+func (llc *LocalLoginChecker) checkLDAPLogin(user string, pwd string) error {
+
+	client, err := ldap.DialURL(Config.Get("auth.ldap.host").AsStr())
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	_, err = client.SimpleBind(&ldap.SimpleBindRequest{
+		Username: "CN=" + user + "," + Config.Get("auth.ldap.dn").AsStr(),
+		Password: pwd,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
